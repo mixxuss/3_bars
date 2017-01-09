db298dfbfb74274243bb9e5e22848b518558e30f
mixxuss <mixxuss@gmail.com>
2017-01-09 17:51:26 +0300

    start

--- bars.py
+++ bars.py
@@ -1,8 +1,17 @@
 import json
+from urllib.request import urlopen
+
+#url='http://op.mos.ru/EHDWSREST/catalog/export/get?id=84505'
+file = 'data-2897-2016-11-23.json'
 
 
 def load_data(filepath):
-    pass
+    # response = urlopen(filepath)
+    # data = response.read()#.decode('utf-8')
+    # return json.loads(data)
+    data = json.loads(filepath)
+    text_data = data.read()
+    return text_data
 
 
 def get_biggest_bar(data):
@@ -18,4 +27,4 @@ def get_closest_bar(data, longitude, latitude):
 
 
 if __name__ == '__main__':
-    pass
+    print(load_data(file))
