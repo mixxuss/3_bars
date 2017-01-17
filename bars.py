e08935fbaedfaadedd49b3442254c5b4515e87da
mixxuss <mixxuss@gmail.com>
2017-01-17 14:48:11 +0300

    Added: new func for making a dictionaries with needed data               filepath from argument               now functions doing something one               .gitignore  Deleted: data from repository

--- bars.py
+++ bars.py
@@ -1,44 +1,47 @@
-import json
+import json, argparse
 from geopy.distance import vincenty
 
 
-def load_data(filepath):
-    # Loads JSON file from filepath variable and return it in str
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filepath', nargs='?')
+    return parser
+
+def load_data_json(filepath):
     with open(filepath) as json_data:
         json_string = json.load(json_data)
     return json_string
 
 
-def get_biggest_bar(json_string):
-    # Gets a str variable with data and return max value key
-    d = {}
-    for bars in json_string:
-        d[bars["Name"]] = bars["SeatsCount"]
-    return max(d, key=d.get)
+def make_dict_seatscount(json_string):
+    return {bars["Name"] : bars["SeatsCount"] for bars in json_string}
 
 
-def get_smallest_bar(json_string):
-    # Gets a str variable with data and return min value key
-    d = {}
-    for bars in json_string:
-        d[bars["Name"]] = bars["SeatsCount"]
-    return min(d, key=d.get)
-
-
-def get_closest_bar(json_string, longitude, latitude):
-    # Gets a str variable with data, longitude and latitude from input and return the closest value key
-    d = {}
+def make_dict_range(json_string, longitude, latitude):
     your_place = (longitude, latitude)
-    for bars in json_string:
-        li = tuple(bars["geoData"]["coordinates"])
-        d[bars["Name"]] = vincenty(li, your_place).kilometers
-    return min(d, key=d.get)
+    dict_range = {bars["Name"] : vincenty(tuple(bars["geoData"]["coordinates"]), your_place).kilometers for bars in json_string}
+    return dict_range
+
+
+def get_biggest_bar(dict_seatscount):
+    return max(dict_seatscount, key=dict_seatscount.get)
+
+
+def get_smallest_bar(dict_seatscount):
+    return min(dict_seatscount, key=dict_seatscount.get)
+
+
+def get_closest_bar(dict_range):
+    return min(dict_range, key=dict_range.get)
 
 
 if __name__ == '__main__':
-    # Put a path to file to filepath variable
-    filepath = 'data-2897-2016-11-23.json'
-    print("Самый большой - ", get_biggest_bar(load_data(filepath)))
-    print("Самый маленький - ", get_smallest_bar(load_data(filepath)))
+    parsed_args = create_parser()
+    filepath = parsed_args.parse_args()
+    loaded_json = load_data_json(filepath)
+    dict_seatscount = make_dict_seatscount(loaded_json)
+    print("Самый большой - ", get_biggest_bar(dict_seatscount))
+    print("Самый маленький - ", get_smallest_bar(dict_seatscount))
     longitude, latitude = float(input("longitude is ")), float(input("latitude is "))
-    print("Самый близкий - ", get_closest_bar(load_data(filepath), longitude, latitude))
+    dict_range = make_dict_range(loaded_json, longitude, latitude)
+    print("Самый близкий - ", get_closest_bar(dict_range))
